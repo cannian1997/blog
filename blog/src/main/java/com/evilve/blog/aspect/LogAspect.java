3958e15a8d4e4a94140f131aa56098d26f324ff6
liukuankuan <201269239@qq.com>
2021-07-17 10:57:18 +0800

    增加异常信息收集

--- blog/src/main/java/com/evilve/blog/aspect/LogAspect.java
+++ blog/src/main/java/com/evilve/blog/aspect/LogAspect.java
@@ -1,7 +1,11 @@
 package com.evilve.blog.aspect;
 
+import com.alibaba.fastjson.JSONObject;
+import com.evilve.blog.annotaction.ExceptionLog;
+import com.evilve.blog.exception.entities.ExceptionInfo;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -9,6 +13,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 @Aspect
@@ -19,6 +24,9 @@ public class LogAspect {
 
     @Pointcut("execution(* com.evilve.blog.web.*.*(..))")
     public void log(){}
+
+    @Pointcut("@annotation(com.evilve.blog.annotaction.ExceptionLog)")
+    public void exceLog(){}
 
 
     @Before("log()")
@@ -43,6 +51,33 @@ public class LogAspect {
     @AfterReturning(returning = "result",pointcut = "log()")
     public void doAfterReturn(Object result){
         logger.info("Result :{}",result);
+    }
+
+
+    @AfterThrowing(value = "exceLog()",throwing = "e")
+    public void afterThrowing(JoinPoint joinPoint, Exception e){
+        ExceptionInfo exceptionInfo = new ExceptionInfo();
+        //获得方法签名
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        //获得执行的某个方法
+        Method method = methodSignature.getMethod();
+        //根据方法获得方法上的注解对象
+        ExceptionLog annotation = method.getAnnotation(ExceptionLog.class);
+        if (annotation != null) {
+            String flag = annotation.exceFlag();
+            if ("Y".equals(flag)) {
+                Object[] args = joinPoint.getArgs();
+                String params = JSONObject.toJSONString(args);
+                String exceEstate = annotation.exceEstate(); //异常等级
+                String exceModule = annotation.exceModule();//异常模块
+                String exceUrl = annotation.exceUrl();//请求路径
+                //封装异常信息落表
+                String message = e.getMessage();//异常信息
+                String messageDetail = e.toString();//异常信息详情
+                //日志唯一编号
+
+            }
+        }
     }
 
     private class RequestLog{
