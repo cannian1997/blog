95bbef23e336035823db3c900e248795c771ec1c
liukuankuan <201269239@qq.com>
2021-07-19 16:21:46 +0800

    修改启动类

--- blog/src/main/java/com/evilve/blog/BlogApplication.java
+++ blog/src/main/java/com/evilve/blog/BlogApplication.java
@@ -1,12 +1,21 @@
 package com.evilve.blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BlogApplication.class);
+    }
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(BlogApplication.class, args);
+        ConfigurableApplicationContext run = new SpringApplicationBuilder(BlogApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
