bcd5f8c73d06a0cf18b69c3ffb8739732a480417
liukuankuan <201269239@qq.com>
2021-07-19 16:16:42 +0800

    修改

--- blog/src/main/java/com/evilve/blog/BlogApplication.java
+++ blog/src/main/java/com/evilve/blog/BlogApplication.java
@@ -2,10 +2,11 @@ package com.evilve.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BlogApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BlogApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(BlogApplication.class, args);
     }
 }
