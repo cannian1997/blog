b5d13eb8b69588b7dea2dd61bfc0338ec62a1bd3
liukuankuan <201269239@qq.com>
2021-07-24 17:16:02 +0800

    修改配置

---
+++ my-blog/blog-gateway/src/main/java/com/evilve/gateway/App.java
@@ -0,0 +1,19 @@
+package com.evilve.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+/**
+ * 启动
+ *
+ * @author <liukuankuan>
+ * @since 2021/7/23 22:38
+ */
+@SpringBootApplication
+@RefreshScope
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
