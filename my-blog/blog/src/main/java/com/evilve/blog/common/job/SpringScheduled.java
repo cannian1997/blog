69b23b52c1cf11102b8b5f5d936ccee764735ecd
liukuankuan <201269239@qq.com>
2021-08-15 15:24:47 +0800

    修改配置

---
+++ my-blog/blog/src/main/java/com/evilve/blog/common/job/SpringScheduled.java
@@ -0,0 +1,21 @@
+package com.evilve.blog.common.job;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * spring定时任务
+ *
+ * @author <liukuankuan>
+ * @since 2021/8/15 14:31
+ */
+@Component
+//@EnableScheduling
+public class SpringScheduled {
+
+//    @Scheduled(cron = "*/5 * * * * *")
+//    public void execute(){
+//        System.out.println("scheduled正在执行!");
+//    }
+}
