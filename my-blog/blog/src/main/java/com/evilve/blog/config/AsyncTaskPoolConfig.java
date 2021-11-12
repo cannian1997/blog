fa0d7ead00d517756ee35119f0e91adf7422e036
liukuankuan <201269239@qq.com>
2021-11-12 14:02:50 +0800

    增加异步任务配置

---
+++ my-blog/blog/src/main/java/com/evilve/blog/config/AsyncTaskPoolConfig.java
@@ -0,0 +1,81 @@
+package com.evilve.blog.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 异步任务线程池配置
+ *
+ * @author <liukuankuan>
+ * @since 2021/9/24 17:59
+ */
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class AsyncTaskPoolConfig implements SchedulingConfigurer, AsyncConfigurer {
+
+    private final Logger log = LoggerFactory.getLogger(AsyncTaskPoolConfig.class);
+
+    /**
+     * 定时任务使用的线程池
+     * @return
+     */
+    @Bean(destroyMethod = "shutdown" , name = "taskScheduler")
+    public ThreadPoolTaskScheduler taskScheduler(){
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("task-");
+        scheduler.setAwaitTerminationSeconds(600);
+        scheduler.setErrorHandler(throwable -> {log.error("调度任务发生异常",throwable);});
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        return scheduler;
+    }
+
+    /**
+     * 异步任务执行线程池
+     * @return
+     */
+    @Bean(name = "asyncExecutor")
+    public ThreadPoolTaskExecutor asyncExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setQueueCapacity(1000);
+        executor.setKeepAliveSeconds(600);
+        executor.setMaxPoolSize(20);
+        executor.setThreadNamePrefix("taskExecutor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        ThreadPoolTaskScheduler taskScheduler = taskScheduler();
+        scheduledTaskRegistrar.setTaskScheduler(taskScheduler);
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return asyncExecutor();
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (throwable,method,objects) -> {
+            log.error("异步任务执行出现异常,message {} ，method {} ,params {}",throwable,method,objects);
+        };
+    }
+}
